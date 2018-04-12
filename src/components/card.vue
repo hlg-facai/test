4ab939e6b0c93b937c1fbd94af647713e00e5d65
facai <facai@gaoding.com>
2018-04-12 18:04:05 +0800

    dev 1

---
+++ src/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p class="card">
+            {{text}}
+        </p>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['text']
+};
+</script>
+
+<style>
+.card {
+    padding: 10px;
+}
+</style>
